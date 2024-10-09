2de40bfe7cc55706074c5ae10391c3b7d065695f
Juan <goheju_aliesma2324@iesmedinaazahara.es>
2024-10-09 11:14:19 +0200

    Subir ejercicios 2 y 3 relación 1

---
+++ Ejercicios/2.py
@@ -0,0 +1,13 @@
+def main(args):
+    numero= int(1)
+    numeront= int(1)
+    for numero in range(1,11):
+        numeront= numero + 1
+        numero == numero + numeront
+        numero == numero + 1
+        print (numeront+numero)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ Ejercicios/3.py
@@ -0,0 +1,13 @@
+
+def main(args):
+    numero= int(input("setear numero 1 "))
+    numero2= int(input("setear numero 2 "))
+    acum = int(1)
+    for numero in range(numero,numero2+1):
+        numero == numero +1
+        print (numero)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
